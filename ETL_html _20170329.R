76b19bdd4a6b24ab1a43a99339a359c2c8109c72
Leong Kaon <leongkaon@gmail.com>
2017-04-03 14:54:24 +0800

    Add files via upload

---
+++ ETL_html _20170329.R
@@ -0,0 +1,245 @@
+# author: leongkaon
+# date: 20170222
+#--------------
+
+library(dplyr)
+library(xml2)
+library(xmlview)
+#--------------------
+
+# 本次內容
+## 1.計算.html每年檔案數
+## 2.每篇報導一個檔案,輸出.txt, 不要標題及「本報訊」,只要內文
+##    計算每個檔案約花時間
+#-------------------
+
+# 1.計算.html每年檔案數
+# wd = "/home/leongkaon/Documents/Text_mining/new_data_html"
+# lf_full_true = list.files(wd, full.names = TRUE, pattern = ".html$")
+# lf_full_false = list.files(wd, full.names = FALSE, pattern = ".html$")
+
+# 刪除所有字串長度奇奇怪怪的檔案
+# 主要因為某些檔案的版數太奇怪，有百幾版，處理也不方便，但資料量少，所以刪除
+## 刪除路徑字元長度不等於中位數的檔案
+# file.remove(
+#         lf_full_true[nchar(lf_full_true) != 
+#                              median(nchar(lf_full_true))]
+#         )
+# 
+# lf_full_true = list.files(wd, full.names = TRUE, pattern = ".html$")
+# lf_full_false = list.files(wd, full.names = FALSE, pattern = ".html$")
+# 
+# file_count = table(substr(lf_full_false,1,4))
+# print(file_count)  # .html每年檔案數
+
+#-------
+
+# 2.每篇報導一個檔案,輸出.txt
+### 刪除內文的\r\n\t及空白
+### 刪除內文的標題
+### 刪除內文【新华社延安十日电】
+
+# 順序:
+# 0.列出所有html檔案
+# 1.讀取某個html檔案
+# 2.刪除檔案的regex符號
+# 3.搜尋html檔中所有報導標題
+# 4.逐篇刪除報導一開始至標題
+# 5.刪除不相關內容，如【新华社延安十日电】
+
+############################################
+## 20170329                               ##
+## 由於111事故不明，我決定沿用自己電腦... ##
+## 不是明智的決定，但這是短期內的可行方案 ##
+## 只選取頭版                             ##
+############################################
+# file input
+wd = "/home/leongkaon/Documents/Text_mining/new_data_html/"
+lf_full_true = list.files(wd, full.names = TRUE, pattern = ".html$")
+lf_full_false = list.files(wd, full.names = FALSE, pattern = ".html$")
+
+# file input pattern YYYYMMDDPP
+pattern = "01.html$" # 是否只用特定年月日的檔案?(regex pattern, example: "^1993" is for year 1993)
+file_path = lf_full_true[grepl(pattern,lf_full_false)]
+write_names = sub(".html","",lf_full_false[grepl(pattern,lf_full_false)]) # 輸出檔案前段名稱
+
+# file output
+file_output = "/home/leongkaon/Documents/Text_mining/people_daily_front_page_txt/"
+
+library(xml2)
+library(xmlview)
+
+## START ##
+errorFile = NULL
+t1 = Sys.time()
+loop_times = 0
+for (i in 154:length(file_path)){
+        loop_times = loop_times + 1
+        if ((loop_times %% 10000)==0){
+                message(loop_times/130000,"%")
+        }
+        doc = read_html(file_path[i])
+        xpath = "//*[@class='article']" # 內文
+        text = xml_text(xml_find_all(doc,xpath))
+        
+        ### 刪除內文的\r\n\t及空白
+        text_no_space = gsub("\r|\t|\n| ","",text)
+        
+        ### 刪除內文的標題及「專欄:」
+        ## 尋找內文中是否有標題,標題起始字元+標題長度 為需刪去內容
+        xpath2 = "//*[@class='box']/h2" #標題
+        title = xml_text(xml_find_all(doc,xpath2))
+        title_no_space = gsub(" ","",title)
+        
+        for (j in 1:length(text_no_space)){
+                # 刪除內文一開始至標題
+                loc = tryCatch(
+                        regexpr(title_no_space[j], text_no_space[j]),
+                        error = function(e) {
+                                cat(file_path[i])
+                                NULL
+                        }
+                )
+                if (is.null(loc)){
+                        errorFile = c(errorFile, file_path[i]);
+                        next
+                }
+                        
+                text_no_title =
+                        tryCatch(
+                                substr(
+                                        text_no_space[j],
+                                        as.numeric(loc) + attr(loc, "match.length"),
+                                        nchar(text_no_space[j])
+                                ),
+                                error = function(e) {
+                                        cat(file_path[i])
+                                        NULL
+                                }
+                        )
+                if (is.null(text_no_title)){
+                        errorFile = c(errorFile, file_path[i]);
+                        next
+                }
+                        
+                
+                ### 刪除內文【新华社延安十日电】# 暫不考慮到"【"可能出現兩次,因資料只有極少量此情況,無謂浪費效能
+                loc1 = regexpr("【", text_no_title)
+                loc2 = regexpr("】", text_no_title)
+                
+                text_clear = sub(substr(text_no_title, loc1, loc2),"",
+                                 text_no_title)
+                
+                writeLines(text_clear,
+                           paste0(file_output,
+                                  write_names[i],
+                                  sprintf("%02d", j),
+                                  ".txt"
+                           )
+                           # encoding problem
+                )
+        }
+}
+Sys.time() - t1
+
+# 個人電腦上運行，初步實驗26個html經整理輸出共300個txt檔，需時0.1836627 secs
+0.1836627/26*130000 # 需時約15分鐘
+300/26 * 130000 # 估計約1,500,000個txt檔
+(812.3 - 509.9) / 812.3 # html檔案共812.3KB,輸出txt後為509.9KB,減少約三成七
+
+# 輸出txt未完全準確，有少數刪除比標題稍長，亦有少數標題與內文標題些許差異,但整體為可接受
+
+length(errorFile); head(errorFile)  #662
+# writeLines(errorFile,'htmlToTxtFail.txt')
+
+a1 = regexpr("01.html$", errorFile)  #616 個txt檔消失，原因最可能為regex字眼
+n_distinct(substr(errorFile, a1 - 8, a1))
+
+
+## BUGs:
+nchar('\U3e34653cº')    # Error
+nchar('U3e34653cº')
+
+## Solve: Ignore
+
+#################################################
+## 有曬頭版堆txt檔，你知架啦，又開始漫長的旅程 ##
+#################################################
+
+
+
+
+
+##############
+## Appendix ##
+##############
+
+国民党当局破坏荷泽协议    蓄意放水淹我解放区    不顾七百万人民生命图逞内战阴谋    中共中央发言人表示坚决反对
+
+国民党当局破坏荷泽协议  蓄意放水淹我解放区  不顾七百万人民生命图逞内战阴谋  中共中央发言人表示坚决反对
+
+
+# 2.每篇報導一個檔案,輸出.txt
+
+## 拎1946測試,.html檔案數為495個
+### 刪除內文的\r\n\t及空白
+### 刪除內文的標題
+### 刪除內文【新华社延安十日电】
+
+file_path = lf_full_true[grepl("^1946",lf_full_false)]
+
+doc = read_html(file_path[1])
+xpath = "//*[@class='article']" # 內文
+text = xml_text(xml_find_all(doc,xpath))
+
+### 刪除內文的\r\n\t及空白
+text_no_space = gsub("\r|\t|\n| ","",text)
+
+### 刪除內文的標題及「專欄:」
+## 尋找內文中是否有標題,標題起始字元+標題長度 為需刪去內容
+xpath2 = "//*[@class='box']/h2" #標題
+title = xml_text(xml_find_all(doc,xpath2))
+title_no_space = gsub(" ","",title)
+
+# 是否找到內文的標題?
+for (i in 1:length(title)){
+        cat(grepl(title_no_space[i], text_no_space[i]), sep="\n")
+}
+
+# 刪除內文一開始至標題
+loc = regexpr(title_no_space[1], text_no_space[1])
+text_no_title =
+        substr(
+                text_no_space[1],
+                as.numeric(loc) + attr(loc, "match.length"),
+                nchar(text_no_space[1])
+        )
+
+### 刪除內文【新华社延安十日电】# 暫不考慮到【可能出現兩次,因資料只有極少量此情況,無謂浪費效能
+loc1 = regexpr("【", text_no_title)
+loc2 = regexpr("】", text_no_title)
+
+text_clear = sub(substr(text_no_title, loc1, loc2),"",
+                 text_no_title)
+
+writeLines(text_clear,"")
+
+##########
+
+
+sub("》","】","AAA》AA")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ crawler.R
@@ -0,0 +1,127 @@
+# 20170208
+# 爬網絡資料,補充人民日報資料
+#--------------------------------
+
+library(dplyr)
+library(ggplot2)
+library(xml2)
+library(xmlview)
+
+# core function
+timesLoop = 0
+#! START # 只要將第一個loop後totalDate[1]改totalDate就可執行，祝自己順利
+totalDate = as.character(seq(as.Date("2002-01-27"), as.Date("2003-12-31"), by="+1 day"))
+system.time(
+        for (date in totalDate){
+                message(date)
+                print(Sys.time())
+                timesLoop = timesLoop + 1
+                # 尋找該日總共多少頁
+                url = paste0("http://www.ziliaoku.org/rmrb/",date)
+                doc = read_html(url)
+                xpath = "//*[@class='box']/h3"
+                a1 = xml_text(xml_find_all(doc,xpath))
+                # 若根本沒有頁數，則直接下一次查詢
+                if (length(a1)==0){
+                        Sys.sleep(rgamma(1,shape = 3, scale = 0.3))
+                        next
+                }
+                pages = as.numeric(gsub("[^0-9]","",a1))
+                ## 休息片刻
+                Sys.sleep(rgamma(1,shape = 3, scale = 0.3))
+                message(pages)
+                # 下載每日每版資料
+                for (page in pages){
+                        message(page)
+                        datePage = paste(date,page,sep = "-")
+                        tryCatch(download.file(paste0("http://www.ziliaoku.org/rmrb/",datePage),
+                                               destfile = paste0("D:/家安的TextMining/new_data_20170208/",
+                                                                 paste0(strsplit(as.character(date),split="-")[[1]],collapse=""),
+                                                                 sprintf("%02d",page),".html")
+                        ),
+                        error = function(e){NULL})
+                        ## 休息片刻 ## 但在凌晨四點至五點稍為加速
+                        if (substr(Sys.time(),12,13) %in% c("4","5")){
+                                Sys.sleep(rgamma(1,shape = 3,scale = 0.6))
+                        } else {
+                                Sys.sleep(rgamma(1,shape = 4,scale = 1))
+                        }
+                }
+                # 約x次休息約分幾鐘
+                if (timesLoop==sample(6:9,1) | timesLoop==10){
+                        Sys.sleep(rgamma(1,shape=50,scale=0.5))
+                        timesLoop = 0
+                }
+                ## 每到十三點休息半個鐘
+                #if (substr(Sys.time(),12,13) %in% c("13")){
+                #        Sys.sleep(3600)
+                #}
+                # 每日到二十點後可以休息耐d..
+                ## 血汗工廠
+                if (as.numeric(substr(Sys.time(),12,13)) %in% c(20:23,0:3)){
+                        Sys.sleep(rgamma(1,shape=23,scale=0.5)+5)
+                }
+        }
+)
+
+# have run seconds
+16488.27 + 5400 + 233941 + 19531.26 + 37011.52 +
+        62875.98 + 4523.98 + 5629.49 + 347774.2 + 50773.41 + 45634.59
+
+################################################################
+
+# 20170221
+# 終於下載完成...
+# 135909個html檔案
+# 但有些檔案得個殼,內容空白...
+
+## file size 單位 bytes
+## 1024 bytes  = 1 KB
+## 1024 KB = 1 MB
+
+file.size("D:/家安的TextMining/new_data_20170208/1997062612.html")
+file.size("D:/家安的TextMining/new_data_20170208/1962012806.html")
+
+lf = list.files("D:/家安的TextMining/new_data_20170208", pattern = ".html$", full.names= TRUE)
+lf2 = list.files("D:/家安的TextMining/new_data_20170208", pattern = ".html$", full.names= FALSE)
+# 找出空白檔案
+empty_file_full = lf[file.size(lf) < 5000]
+empty_file = lf2[file.size(lf) < 5000]
+tmp = sub(".html","",empty_file)
+empty_date_page = paste(substr(tmp,1,4),substr(tmp,5,6),
+                        substr(tmp,7,8),substr(tmp,9,nchar(tmp)),sep = "-")
+
+# core function改
+system.time(
+
+                # 下載每日每版資料
+                for (i in 1:length(empty_date_page)){
+                        cat(empty_date_page[i])
+                        tryCatch(download.file(paste0("http://www.ziliaoku.org/rmrb/",empty_date_page[i]),
+                                               destfile = empty_file_full[i]),
+                        error = function(e){NULL})
+                        ## 休息片刻 ## 但在凌晨四點至五點稍為加速
+                        if (substr(Sys.time(),12,13) %in% c("4","5")){
+                                Sys.sleep(rgamma(1,shape = 3,scale = 0.6))
+                        } else {
+                                Sys.sleep(rgamma(1,shape = 4,scale = 1))
+                        }
+                }
+
+)
+
+# run time seconds
+15856.61
+
+###################################################
+
+# 1959102202 檔案夠大,但都係唔完整,無尾行
+
+
+
+
+
+
+
+
+
